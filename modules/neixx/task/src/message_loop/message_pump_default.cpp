32a4c2d3e228dd30dea5162242e9b39586571518
ylf <ylf-star@hotmail.com>
2026-05-12 20:42:41 +0800

    test(task): simplify nested run tests and delayed deadline handling

--- modules/neixx/task/src/message_loop/message_pump_default.cpp
+++ modules/neixx/task/src/message_loop/message_pump_default.cpp
@@ -82,14 +82,11 @@ class MessagePumpDefault::Impl {
   void UpdateDelayedWorkFromDelegate(const Delegate::NextWorkInfo& next_work_info) {
     AutoLock lock(state_lock_);
     if (next_work_info.next_run_time == Delegate::NextWorkInfo::kNoScheduledRunTime) {
-      // Do not clear existing deadline: an outer Run() may have a valid
-      // delayed deadline that must survive an inner Run() returning no work.
+      has_delayed_run_time_ = false;
       return;
     }
-    if (!has_delayed_run_time_ || next_work_info.next_run_time < delayed_run_time_) {
-      delayed_run_time_ = next_work_info.next_run_time;
-      has_delayed_run_time_ = true;
-    }
+    delayed_run_time_ = next_work_info.next_run_time;
+    has_delayed_run_time_ = true;
   }
 
   bool ConsumeWorkScheduled() {
